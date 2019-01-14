d1aeaf053dcff4359e66d544cf11e4a7912cd85e
Eric Fueng <efueng@eagles.ewu.edu>
2019-01-13 22:03:38 -0800

    Initial commit to make sure build is working properly

---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/UserTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Tests.Models
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void CreateUser()
+        {
+            User user = new User { FirstName = "Edmond", LastName = "Dantes" };
+            Assert.AreEqual("Edmond", user.FirstName);
+
+        }
+    }
+}
