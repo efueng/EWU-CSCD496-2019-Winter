59e7d9d6487a9646a6092b4232fc636f5d421eda
Eric Lin <efueng@eagles.ewu.edu>
2019-01-16 22:09:18 -0800

    Changes to Group and Message
    Removed unnecessary methods from Group and added a message Body for Message

--- SecretSanta/src/SecretSanta.Domain/Models/Group.cs
+++ SecretSanta/src/SecretSanta.Domain/Models/Group.cs
@@ -6,52 +6,23 @@ namespace SecretSanta.Domain.Models
 {
     public class Group : Entity
     {
-        public Group(string title)
-        {
-            Title = title;
-            Users = new List<User>();
-        }
-
         private string _Title;
         public string Title
         {
             get => _Title;
             set
             {
-                if (value is null)
+                if (string.IsNullOrEmpty(value))
                 {
-                    throw new ArgumentNullException("value");
+                    throw new ArgumentNullException("Title cannot be null or empty.", "value");
                 }
 
                 value = value.Trim();
-
-                if (value is "")
-                {
-                    throw new ArgumentException("Title cannot be empty.", "value");
-                }
-
                 _Title = value;
             }
         }
         
         public ICollection<User> Users { get; set; }
-
-        public void AddUser(string firstName, string lastName)
-        {
-            User user = new User(firstName, lastName);
-            Users.Add(user);
-        }
-
-        public void RemoveUser(string firstName, string lastName)
-        {
-            foreach(User u in Users)
-            {
-                if (u.FirstName.Equals(firstName) && u.LastName.Equals(lastName))
-                {
-                    Users.Remove(u);
-                }
-            }
-        }
-
     }
 }
+
