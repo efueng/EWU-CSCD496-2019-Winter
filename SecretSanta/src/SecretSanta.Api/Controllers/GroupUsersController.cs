42717ab3dbc20599f8c790b3922a90b28e1f6819
Eric <efueng@hotmail.com>
2019-02-14 20:07:59 -0800

    Quick fix
    Commented out the in-progress GroupUserController and related classes to see if the CI/CD pipeline works

--- SecretSanta/src/SecretSanta.Api/Controllers/GroupUsersController.cs
+++ SecretSanta/src/SecretSanta.Api/Controllers/GroupUsersController.cs
@@ -1,66 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using SecretSanta.Api.Services.Interfaces;
-using SecretSanta.Domain.Services.Interfaces;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using AutoMapper;
+//using Microsoft.AspNetCore.Mvc;
+//using SecretSanta.Api.Services.Interfaces;
+//using SecretSanta.Domain.Services.Interfaces;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace SecretSanta.Api.Controllers
-{
-    [Route("api/[controller]")]
-    public class GroupUsersController : ControllerBase
-    {
-        private IGroupUserService GroupService { get; set; }
-        private IMapper Mapper { get; set; }
+//namespace SecretSanta.Api.Controllers
+//{
+//    [Route("api/[controller]")]
+//    public class GroupUsersController : ControllerBase
+//    {
+//        private IGroupUserService GroupService { get; set; }
+//        private IMapper Mapper { get; set; }
 
-        public GroupUsersController(IGroupUserService groupService, IMapper mapper)
-        {
-            GroupService = groupService;
-            Mapper = mapper;
-        }
+//        public GroupUsersController(IGroupUserService groupService, IMapper mapper)
+//        {
+//            GroupService = groupService;
+//            Mapper = mapper;
+//        }
 
-        [HttpPut("{groupId}")]
-        public async Task<IActionResult> AddUserToGroup(int groupId, int userId)
-        {
-            if (groupId <= 0)
-            {
-                return BadRequest();
-            }
+//        [HttpPut("{groupId}")]
+//        public async Task<IActionResult> AddUserToGroup(int groupId, int userId)
+//        {
+//            if (groupId <= 0)
+//            {
+//                return BadRequest();
+//            }
 
-            if (userId <= 0)
-            {
-                return BadRequest();
-            }
+//            if (userId <= 0)
+//            {
+//                return BadRequest();
+//            }
 
-            if (await GroupService.AddUserToGroup(groupId, userId))
-            {
-                return Ok();
-            }
-            return NotFound();
-        }
+//            if (await GroupService.AddUserToGroup(groupId, userId))
+//            {
+//                return Ok();
+//            }
+//            return NotFound();
+//        }
 
-        [HttpDelete("{groupId}")]
-        public async Task<IActionResult> RemoveUserFromGroup(int groupId, int userId)
-        {
-            if (groupId <= 0)
-            {
-                return BadRequest();
-            }
+//        [HttpDelete("{groupId}")]
+//        public async Task<IActionResult> RemoveUserFromGroup(int groupId, int userId)
+//        {
+//            if (groupId <= 0)
+//            {
+//                return BadRequest();
+//            }
 
-            if (userId <= 0)
-            {
-                return BadRequest();
-            }
+//            if (userId <= 0)
+//            {
+//                return BadRequest();
+//            }
 
-            if (await GroupService.RemoveUserFromGroup(groupId, userId))
-            {
-                return Ok();
-            }
-            return NotFound();
-        }
-    }
-}
+//            if (await GroupService.RemoveUserFromGroup(groupId, userId))
+//            {
+//                return Ok();
+//            }
+//            return NotFound();
+//        }
+//    }
+//}
