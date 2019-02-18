09d8844352bd1245df046408705370a94343632b
Eric <efueng@hotmail.com>
2019-02-17 18:56:40 -0800

    New migration
    new migration for the Pairings

---
+++ SecretSanta/src/SecretSanta.Domain/Migrations/20190218025339_AddedPairingsMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecretSanta.Domain.Migrations
+{
+    public partial class AddedPairingsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OriginGroupId",
+                table: "Pairings",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginGroupId",
+                table: "Pairings");
+        }
+    }
+}
