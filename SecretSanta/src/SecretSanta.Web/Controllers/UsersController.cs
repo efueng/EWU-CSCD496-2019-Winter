0d60eb606dbf8b379a4c6777a5d7f792abc9b42e
Eric <efueng@hotmail.com>
2019-02-24 21:42:19 -0800

    Added NSwag generated InputViewModels

--- SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SecretSanta.Web.ApiModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,10 +13,53 @@ namespace SecretSanta.Web.Controllers
 {
     public class UsersController : Controller
     {
+        private IHttpClientFactory ClientFactory { get; }
+        public UsersController(IHttpClientFactory clientFactory)
+        {
+            ClientFactory = clientFactory;
+        }
+
         // GET: /<controller>/
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Users = await secretSantaClient.GetAllUsersAsync();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(UserInputViewModel viewModel)
+        {
+            IActionResult result = View();
+
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+                {
+                    try
+                    {
+                        var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                        await secretSantaClient.CreateUserAsync(viewModel);
+
+                        result = RedirectToAction(nameof(Index));
+                    }
+                    catch(SwaggerException se)
+                    {
+                        ViewBag.ErrorMessage = se.Message;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
