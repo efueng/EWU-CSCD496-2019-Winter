fadedb4770c8571077a318abbb1f52474e5c1d7b
Eric <efueng@hotmail.com>
2019-03-07 19:56:05 -0800

    initial work

---
+++ SecretSanta/test/SecretSanta.Web.UITests/GroupPageTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace SecretSanta.Web.UITests
+{
+    [TestClass]
+    public class GroupPageTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            //IWebDriver
+        }
+        [TestMethod]
+        public void CanAddGroup()
+        {
+        }
+    }
+}
